fe0375d5afb0ef47ce995ef2a3f5ca890e8f1807
block-doc-au <neilc@cns-gc.com.au>
2022-08-16 08:02:06 +1000

    NC completed scope lab

--- index.js
+++ index.js
@@ -1,1 +1,61 @@
-// Write your solution in this file!
+/*
+
+customerName = 'bob';
+const leastFavoriteCustomer = `Fred`;
+
+
+function upperCaseCustomerName(theName){
+    let convertedName = (theName.toUpperCase());
+       return convertedName;
+   
+};
+
+
+function setBestCustomer(theName){
+ bestCustomer = 'not bob';
+ return bestCustomer;
+};
+
+function overwriteBestCustomer(theName){
+bestCustomer = theName;
+return bestCustomer;
+};
+
+function changeLeastFavoriteCustomer(theName){
+    leastFavoriteCustomer = theName;
+return leastFavoriteCustomer; 
+};
+
+
+// Neil's test for node...
+
+let thisName = upperCaseCustomerName(customerName);
+console.log(`Finally... ${thisName}.`);
+*/
+customerName = 'bob';
+const leastFavoriteCustomer = `Fred`;
+
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+    return bestCustomer;
+   };
+
+
+   function upperCaseCustomerName(){
+    //let convertedName = (customerName.toUpperCase());
+     //  return convertedName;
+       customerName = customerName.toUpperCase();
+       return customerName;
+   
+};
+
+function overwriteBestCustomer(theName){
+    bestCustomer = theName;
+    return bestCustomer;
+    };
+    
+    function changeLeastFavoriteCustomer(theName){
+        leastFavoriteCustomer = theName;
+    return leastFavoriteCustomer; 
+    };
+
